8f2336f7a343e60b6c094dbf816cc06a1b58216e
jiedevm <dieudonne.allognon@yahoo.com>
2022-05-02 16:20:02 +0200

    Serving pages from public/pages folder

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const BASE_URL = "/";
 
 const METHOD_GET = "GET";
 
 const server = http.createServer((req, res) => {
+    const pagesPath = path.resolve(__dirname, "public", "pages");
+
+    let output = "";
+
     try {
         if (req.method === METHOD_GET && req.url === BASE_URL) {
             res.writeHead(200, { "content-type": "text/html" });
-            res.write("<h1>HELLO WORLD Dieudonné !</h1>");
+            output = fs.readFileSync(path.resolve(pagesPath, "index.html"));
         } else {
             res.writeHead(404, { "content-type": "text/html" });
-            res.write("<h1>404 Page introuvale !</h1>");
+            output = fs.readFileSync(path.resolve(pagesPath, "error_404.html"));
         }
     } catch (e) {
         res.writeHead(500, { "content-type": "text/html" });
-        res.write("<h1>500 Erreur Interne au Serveur !</h1>");
+        output = fs.readFileSync(path.resolve(pagesPath, "error_500.html"));
     }
+    res.write(output);
     res.end();
 });
 
