8badb8d0bd26a97b391f541d2c63ea5f88f4a266
jiedevm <dieudonne.allognon@yahoo.com>
2022-05-02 16:00:59 +0200

    Add response when accesssing undefined route

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const server = http.createServer((req, res) => {
     if (req.method === METHOD_GET && req.url === BASE_URL) {
         res.writeHead(200, { "content-type": "text/html" });
         res.write("<h1>HELLO WORLD Dieudonné !</h1>");
+    } else {
+        res.writeHead(404, { "content-type": "text/html" });
+        res.write("<h1>404 Page introuvale !</h1>");
     }
     res.end();
 });
