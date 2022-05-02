2a867fbbfc63cb8c480646921ac9ce6f2c53ea73
jiedevm <dieudonne.allognon@yahoo.com>
2022-05-02 16:05:51 +0200

    Handle Internal Server errors

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const BASE_URL = "/";
 const METHOD_GET = "GET";
 
 const server = http.createServer((req, res) => {
-    if (req.method === METHOD_GET && req.url === BASE_URL) {
-        res.writeHead(200, { "content-type": "text/html" });
-        res.write("<h1>HELLO WORLD Dieudonné !</h1>");
-    } else {
-        res.writeHead(404, { "content-type": "text/html" });
-        res.write("<h1>404 Page introuvale !</h1>");
+    try {
+        if (req.method === METHOD_GET && req.url === BASE_URL) {
+            res.writeHead(200, { "content-type": "text/html" });
+            res.write("<h1>HELLO WORLD Dieudonné !</h1>");
+        } else {
+            res.writeHead(404, { "content-type": "text/html" });
+            res.write("<h1>404 Page introuvale !</h1>");
+        }
+    } catch (e) {
+        res.writeHead(500, { "content-type": "text/html" });
+        res.write("<h1>500 Erreur Interne au Serveur !</h1>");
     }
     res.end();
 });
