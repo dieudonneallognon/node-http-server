e8c581325bc824e3a3ab5a1a6d48aace091af671
jiedevm <dieudonne.allognon@yahoo.com>
2022-05-02 15:53:21 +0200

    Add html rendering on GET / route

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const http = require("http");
 
+const BASE_URL = "/";
+
+const METHOD_GET = "GET";
+
 const server = http.createServer((req, res) => {
-    res.write("HELLO WORLD Dieudonné");
+    if (req.method === METHOD_GET && req.url === BASE_URL) {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.write("<h1>HELLO WORLD Dieudonné !</h1>");
+    }
     res.end();
 });
 
