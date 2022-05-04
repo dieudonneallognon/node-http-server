a2dbe06807784d5303caeff237ba173658dfa2cd
jiedevm <dieudonne.allognon@yahoo.com>
2022-05-04 11:05:58 +0200

    Add POST name route

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const fs = require("fs");
 
 const BASE_URL = "/";
 
-const METHOD_GET = "GET";
+const METHOD = {
+    GET: "GET",
+    POST: "POST",
+};
 
 const ASSETS = {
     REGEX: /\/public\/[A-Za-z0-9]+\/[A-Za-z0-9]+[.][A-Za-z0-9]+/,
@@ -23,53 +26,92 @@ MEMORY_DB = {
     DATA: new Map(),
 };
 
-const server = http.createServer((req, res) => {
-    MEMORY_DB.DATA.set(MEMORY_DB.INDEX++, { nom: "Alice" }); // voici comment set une nouvelle entrée.
-    MEMORY_DB.DATA.set(MEMORY_DB.INDEX++, { nom: "Bob" });
-    MEMORY_DB.DATA.set(MEMORY_DB.INDEX++, { nom: "Charlie" });
+MEMORY_DB.DATA.set(MEMORY_DB.INDEX++, { nom: "Alice" }); // voici comment set une nouvelle entrée.
+MEMORY_DB.DATA.set(MEMORY_DB.INDEX++, { nom: "Bob" });
+MEMORY_DB.DATA.set(MEMORY_DB.INDEX++, { nom: "Charlie" });
 
+const server = http.createServer((req, res) => {
     const pagesPath = path.resolve(__dirname, "public", "pages");
 
     let output = "";
 
     try {
-        if (req.method === METHOD_GET && req.url === BASE_URL) {
-            res.writeHead(200, { "content-type": ASSETS.CONTENT_TYPES.html });
-            output = fs.readFileSync(path.resolve(pagesPath, "index.html"));
-        } else if (req.method === METHOD_GET && req.url.match(ASSETS.REGEX)) {
-            res.writeHead(200, {
-                "content-type": ASSETS.CONTENT_TYPES[req.url.split(".").pop()],
-            });
-            output = fs.readFileSync(
-                path.resolve(__dirname, ...req.url.split("/"))
-            );
-        } else if (req.method === METHOD_GET && req.url === "/api/names") {
-            res.writeHead(200, {
-                "content-type": ASSETS.CONTENT_TYPES.json,
-            });
-            output = JSON.stringify(Array.from(MEMORY_DB.DATA));
-        } else if (
-            req.method === METHOD_GET &&
-            req.url.match(/\/api\/names\/[0-9]+/)
-        ) {
-            res.writeHead(200, {
-                "content-type": ASSETS.CONTENT_TYPES.json,
-            });
+        switch (req.method) {
+            case METHOD.GET: {
+                if (req.url === BASE_URL) {
+                    res.writeHead(200, {
+                        "content-type": ASSETS.CONTENT_TYPES.html,
+                    });
+                    output = fs.readFileSync(
+                        path.resolve(pagesPath, "index.html")
+                    );
+                } else if (req.url.match(ASSETS.REGEX)) {
+                    res.writeHead(200, {
+                        "content-type":
+                            ASSETS.CONTENT_TYPES[req.url.split(".").pop()],
+                    });
+                    output = fs.readFileSync(
+                        path.resolve(__dirname, ...req.url.split("/"))
+                    );
+                } else if (req.url === "/api/names") {
+                    res.writeHead(200, {
+                        "content-type": ASSETS.CONTENT_TYPES.json,
+                    });
+                    output = JSON.stringify(Array.from(MEMORY_DB.DATA));
+                } else if (req.url.match(/\/api\/names\/[0-9]+/)) {
+                    res.writeHead(200, {
+                        "content-type": ASSETS.CONTENT_TYPES.json,
+                    });
 
-            output = JSON.stringify(
-                MEMORY_DB.DATA.get(Number(req.url.split("/").pop())) ?? {}
-            );
-        } else {
-            res.writeHead(404, { "content-type": "text/html" });
-            output = fs.readFileSync(path.resolve(pagesPath, "error_404.html"));
+                    output = JSON.stringify(
+                        MEMORY_DB.DATA.get(Number(req.url.split("/").pop())) ??
+                            {}
+                    );
+                } else {
+                    res.writeHead(404, { "content-type": "text/html" });
+                    output = fs.readFileSync(
+                        path.resolve(pagesPath, "error_404.html")
+                    );
+                }
+                res.write(output);
+                res.end();
+                break;
+            }
+            case METHOD.POST: {
+                if (req.url === "/api/names") {
+                    let data = "";
+                    req.on("data", (chunk) => {
+                        data += chunk;
+                    });
+                    req.on("end", () => {
+                        res.writeHead(201, {
+                            "content-type": ASSETS.CONTENT_TYPES.json,
+                        });
+
+                        MEMORY_DB.DATA.set(
+                            MEMORY_DB.INDEX++,
+                            JSON.parse(data).name
+                        );
+
+                        res.write(
+                            JSON.stringify({
+                                id: MEMORY_DB.INDEX - 1,
+                                name:
+                                    MEMORY_DB.DATA.get(MEMORY_DB.INDEX - 1) ??
+                                    "",
+                            })
+                        );
+
+                        res.end();
+                    });
+                }
+            }
         }
     } catch (e) {
         console.log(e);
         res.writeHead(500, { "content-type": "text/html" });
-        output = fs.readFileSync(path.resolve(pagesPath, "error_500.html"));
+        res.write(fs.readFileSync(path.resolve(pagesPath, "error_500.html")));
     }
-    res.write(output);
-    res.end();
 });
 
 server.listen(5000);
